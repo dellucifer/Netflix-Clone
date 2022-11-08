942d6a0d9c6c560504e0db2f1ab7f5826d85f2ba
Dellucifer <pg99285@gmail.com>
2022-11-09 00:26:40 +0530

    Added Login Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginScreen from './components/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="App">
-      {/* <h1>Hi</h1> */}
-      <HomeScreen />
+
+      <Router>
+        {!user ? (<LoginScreen />) : (
+          <Routes>
+          <Route exact path="/" component={HomeScreen} />
+        </Routes>
+        )}
+    </Router>
     </div>
   );
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import "./Row.css";
 import axios from "./axios";
 
-function Row({ title, fetchURL, isLargeRow = false }) {
+function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original";
 
@@ -28,11 +28,7 @@ function Row({ title, fetchURL, isLargeRow = false }) {
               <img
                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                 key={movie.id}
-                src={`${base_url}${
-                  isLargeRow ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt={movie.name}
-              />
+                src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
             )))
         )}
       </div>
